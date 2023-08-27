253b2aadeb9e7bc427c4374c36f95db01ad6798a
Bokeron8 <franco123rex777@gmail.com>
2023-08-27 15:54:57 -0300

    Added bus icon

--- src/lib/colectivos.js
+++ src/lib/colectivos.js
@@ -1,3 +1,4 @@
+import L from "leaflet";
 export async function getStopPointsByLine(linea) {
     const response = await fetch(`/api/get-stop-points?linea=${linea}`)
     const data = await response.json()
@@ -8,7 +9,7 @@ export async function getArrives(linea, parada) {
     const response = await fetch(`/api/get-arrives?linea=${linea}&parada=${parada}`)
     const data = await response.json()
 
-    return data.arribos;
+    return data.arribos ? data.arribos : [];
 }
 export async function getLineRoute(linea) {
     const response = await fetch(`/api/get-route?linea=${linea}`)
@@ -24,3 +25,11 @@ export async function getNearestStops(lat, lng) {
     return data.paradas;
 }
 
+
+const iconURL = new URL('images/colectivo-base.png', import.meta.url).href
+export let myIcon = L.icon({
+    iconUrl: iconURL,
+    iconSize: [20, 20],
+    iconAnchor: [10, 20],
+    popupAnchor: [0, -20],
+});
